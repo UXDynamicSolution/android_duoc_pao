80d25a97d2ca1f71498c8d3fa139f677368f4676
cmunoz_dev <cah.munoz.ti@gmail.com>
2025-08-26 22:13:56 -0400

    arrays, listValues, package utils

--- ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity2.kt
+++ ProyectoBase/app/src/main/java/com/example/proyectobase/MainActivity2.kt
@@ -2,8 +2,11 @@ package com.example.proyectobase
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,11 +20,40 @@ MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         val btnCalculadora: Button = findViewById(R.id.btn_abrir_calculadora)
 
+        //definimos un array
+        val menuOpciones = listOf("Matemáticas", "Lenguaje", "Historia", "Inglés")
+
+        //Buscamos el elemento lista del activity
+        val lsMenu: ListView = findViewById(R.id.ls_mensajes)
+
+        // Se crea un adaptador (ArrayAdapter) que sirve de puente
+        // entre un array/lista de Strings (menuOpciones) y el ListView en pantalla.
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            menuOpciones
+        )
+        //se inicializa el elememento menu
+        lsMenu.adapter = adapter
+
+        lsMenu.setOnItemClickListener { _, _, position, _ ->
+            val seleccionado = menuOpciones[position]
+            Toast.makeText(this, "Elegiste: $seleccionado", Toast.LENGTH_SHORT).show()
+
+            //se abre un activity segun la posicion
+            if(seleccionado == menuOpciones[0]){
+                val abrirCalculadora = Intent(this, MainActivity4::class.java)
+                startActivity(abrirCalculadora)
+            }
+        }
+
+
         btnCalculadora.setOnClickListener{
             val abrirCalculadora = Intent(this, MainActivity4::class.java)
             startActivity(abrirCalculadora)
 
         }
+
 
 
         //ACTIVITY DESTINO
